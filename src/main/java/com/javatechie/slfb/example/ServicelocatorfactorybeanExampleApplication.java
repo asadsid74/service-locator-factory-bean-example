ec9a386d0a569133d1ed8b44df68c61294fc6e14
Asadsid <er.asad.tech@gmail.com>
2021-12-16 09:28:36 -0800

    avoid lombok

--- src/main/java/com/javatechie/slfb/example/ServicelocatorfactorybeanExampleApplication.java
+++ src/main/java/com/javatechie/slfb/example/ServicelocatorfactorybeanExampleApplication.java
@@ -2,12 +2,13 @@ package com.javatechie.slfb.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ServicelocatorfactorybeanExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServicelocatorfactorybeanExampleApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(ServicelocatorfactorybeanExampleApplication.class, args);
 	}
 
 }
--- src/main/java/com/javatechie/slfb/example/dto/PaymentRequest.java
+++ src/main/java/com/javatechie/slfb/example/dto/PaymentRequest.java
@@ -2,9 +2,34 @@ package com.javatechie.slfb.example.dto;
 
 import lombok.Data;
 
-@Data
+
 public class PaymentRequest {
 	private long amount;
 	private String paymentMethod;
+	public PaymentRequest(long amount, String paymentMethod) {
+		super();
+		this.amount = amount;
+		this.paymentMethod = paymentMethod;
+	}
+	public PaymentRequest() {
+		
+	}
+	public long getAmount() {
+		return amount;
+	}
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	@Override
+	public String toString() {
+		return "PaymentRequest [amount=" + amount + ", paymentMethod=" + paymentMethod + "]";
+	}
 
+	
 }
